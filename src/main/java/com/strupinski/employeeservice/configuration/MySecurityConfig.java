f813c73a303e64514f51e0c1eccedd7e7a6c1777
EgorStrupinskiy <venik2405@yandex.ru>
2023-03-15 10:54:25 +0300

    Authentication moved to database storage

--- src/main/java/com/strupinski/employeeservice/configuration/MySecurityConfig.java
+++ src/main/java/com/strupinski/employeeservice/configuration/MySecurityConfig.java
@@ -16,21 +16,21 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().dataSource(dataSource);
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder
-                        .username("egor")
-                        .password("egor")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder
-                        .username("vlad")
-                        .password("vlad")
-                        .roles("HR"))
-                .withUser(userBuilder
-                        .username("ivan")
-                        .password("ivan")
-                        .roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder
+//                        .username("egor")
+//                        .password("egor")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder
+//                        .username("vlad")
+//                        .password("vlad")
+//                        .roles("HR"))
+//                .withUser(userBuilder
+//                        .username("ivan")
+//                        .password("ivan")
+//                        .roles("MANAGER", "HR"));
 
     }
 
